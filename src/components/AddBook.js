5581ec79d96f1fef84f85d9f589d16d4b556d8a2
AhmadiGu <ahmadigulalai76@gmail.com>
2022-12-24 12:27:55 +0500

    Components and pages added

---
+++ src/components/AddBook.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const AddBook = () => (
+  <div>
+    <h2 className="form-title"> ADD NEW BOOK</h2>
+    <form className="add-form">
+      <input className="form-item add-title" type="text" placeholder="Book title" />
+      <select className="form-item add-category" name="category">
+        <option className="disable">Category</option>
+        <option value="Action">Action</option>
+        <option value="Science Fiction">Science Fiction</option>
+        <option value="Economy">Economy</option>
+      </select>
+      <button type="submit" className="add-btn">Add Book</button>
+    </form>
+  </div>
+);
+
+export default AddBook;
