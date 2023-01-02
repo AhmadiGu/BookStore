9fcdd3cc227fe5529f98d233007abf20544ee266
AhmadiGu <ahmadigulalai76@gmail.com>
2023-01-02 15:44:38 +0500

    Add book actions, state and reducer

---
+++ src/redux/book/book.js
@@ -0,0 +1,36 @@
+const ADD_BOOK = 'ADD_BOOK';
+const REMOVE_BOOK = 'REMOVE_BOOK';
+
+export const addBook = (book) => ({
+  type: ADD_BOOK,
+  payload: book,
+});
+
+export const removeBook = (id) => ({
+  type: REMOVE_BOOK,
+  payload: id,
+});
+
+const initialState = {
+  books: [],
+};
+
+const bookReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_BOOK:
+      return {
+        ...state,
+        books: [...state.books, action.payload],
+      };
+
+    case REMOVE_BOOK:
+      return {
+        ...state,
+        books: state.books.filter((book) => book !== action.payload),
+      };
+
+    default:
+      return state;
+  }
+};
+export default bookReducer;
