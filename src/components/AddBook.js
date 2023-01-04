299e37086b20c0087f16c27710b6110727e7730a
AhmadiGu <ahmadigulalai76@gmail.com>
2023-01-04 21:37:26 +0500

    Add and remove action use in components

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,20 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addBook } from '../redux/book/book';
 
-const AddBook = () => (
-  <div>
-    <h2 className="form-title"> ADD NEW BOOK</h2>
-    <form className="add-form">
-      <input className="form-item add-title" type="text" placeholder="Book title" />
-      <input className="form-item add-author" type="text" placeholder="Book author" />
-      <select className="form-item add-category" name="category">
-        <option className="disable">Category</option>
-        <option value="Action">Action</option>
-        <option value="Science Fiction">Science Fiction</option>
-        <option value="Economy">Economy</option>
-      </select>
-      <button type="submit" className="add-btn">Add Book</button>
-    </form>
-  </div>
-);
+const AddBook = () => {
+  const dispatch = useDispatch();
+  const [title, setBookName] = useState('');
+  const [author, setAutherName] = useState('');
+  const [id, setBookId] = useState('');
+  const [category, setCategory] = useState('');
+
+  const prevDefault = (e) => {
+    e.preventDefault();
+    setBookName('');
+    setAutherName('');
+    setBookId(Date.now());
+  };
+
+  return (
+    <div>
+      <h2 className="form-title"> ADD NEW BOOK</h2>
+      <form className="add-form" onSubmit={(e) => prevDefault(e)}>
+        <input
+          className="form-item add-title"
+          type="text"
+          placeholder="Book title"
+          value={title}
+          onChange={(e) => setBookName(e.target.value)}
+        />
+        <input
+          className="form-item add-author"
+          type="text"
+          placeholder="Book author"
+          value={author}
+          onChange={(e) => setAutherName(e.target.value)}
+        />
+        <select
+          className="form-item add-category"
+          name="category"
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+        >
+          <option className="disable">Category</option>
+          <option value="Action">Action</option>
+          <option value="Science Fiction">Science Fiction</option>
+          <option value="Economy">Economy</option>
+        </select>
+        <button
+          type="button"
+          className="add-btn"
+          onClick={() => dispatch(addBook(id, title, author, category))}
+        >
+          Add Book
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default AddBook;
--- src/redux/book/book.js
+++ src/redux/book/book.js
@@ -1,18 +1,35 @@
 const ADD_BOOK = 'BookStore/book/ADD_BOOK';
 const REMOVE_BOOK = 'BookStore/book/REMOVE_BOOK';
 
-export const addBook = (book) => ({
-  type: ADD_BOOK,
-  payload: book,
-});
-
-export const removeBook = (id) => ({
-  type: REMOVE_BOOK,
-  payload: id,
-});
-
 const initialState = {
-  books: [],
+  books: [
+
+    {
+      id: 1,
+      title: 'The Hunger Games',
+      author: 'Suzanne Collins',
+      category: 'Action',
+      currentChapter: 'Chapter 17',
+      progress: 64,
+    },
+    {
+      id: 2,
+      title: 'Dune',
+      author: 'Frank Herbert',
+      category: 'Science Fiction',
+      currentChapter: 'Chapter 3: Lesson Learned',
+      progress: 6,
+    },
+    {
+      id: 3,
+      title: 'Capital in the Twenty-First Century',
+      author: 'Suzanne Collins',
+      category: 'Action',
+      currentChapter: 'Chapter 17',
+      progress: 0,
+    },
+
+  ],
 };
 
 const bookReducer = (state = initialState, action) => {
@@ -26,11 +43,23 @@ const bookReducer = (state = initialState, action) => {
     case REMOVE_BOOK:
       return {
         ...state,
-        books: state.books.filter((book) => book !== action.payload),
+        books: state.books.filter((book) => book.id !== action.payload),
       };
 
     default:
       return state;
   }
 };
+export const addBook = (id, title, author, category) => ({
+  type: ADD_BOOK,
+  payload: {
+    id, title, author, category,
+  },
+});
+
+export const removeBook = (id) => ({
+  type: REMOVE_BOOK,
+  payload: id,
+});
+
 export default bookReducer;
